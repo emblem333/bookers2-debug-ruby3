b4dfef4fcd39ed7c3b4624d4faa4e0d2f15fdfc3
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-03-07 14:47:39 +0000

    [Add]task6-asynchronous

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,25 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
 
-    comment = current_user.book_comments.new(book_comment_params)
+    @comment = current_user.book_comments.new(book_comment_params)
       #↑と同じ意味comment = PostComment.new(post_comment_params)
     #↑と同じ意味comment.user_id = current_user.id
 
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @comment.book_id = @book.id
+    @comment.save
+    render :asynchronous_form
+    #redirect_to request.referer
     #redirect_to request.refererを使うと簡単に同じページに遷移 #book_path(book)
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    flash.now[:alert] = '投稿を削除しました'
+    #renderしたときに@postのデータがないので@postを定義
+    @book = Book.find(params[:book_id])
+    render :asynchronous_form
+    #redirect_to request.referer
   end
 
   private
